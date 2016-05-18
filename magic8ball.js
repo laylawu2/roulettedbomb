7299b6bfa81da20f30c894f4d8dffc99dcde92ba
Layla Wu <hiro0404@gmail.com>
2016-05-18 07:51:19 -0700

    Add files via upload

---
+++ magic8ball.js
@@ -0,0 +1,42 @@
+function answer() {var number = Math.floor(Math.random()*20);
+
+if (number===0){document.getElementById("answer_field").innerHTML = "It is certain";}
+if (number===1){document.getElementById("answer_field").innerHTML = "It is decidedly so";}
+if (number===2) {document.getElementById("answer_field").innerHTML = "Without a doubt";}
+if (number===3) {document.getElementById("answer_field").innerHTML = "Yes, definitely";}
+if (number===4) {document.getElementById("answer_field").innerHTML = "You may rely on it";}
+if (number===5) {document.getElementById("answer_field").innerHTML = "As I see it, yes";}
+if (number===6) {document.getElementById("answer_field").innerHTML = "Most likely";}
+if (number===7) {document.getElementById("answer_field").innerHTML = "Outlook good";}
+if (number===8){document.getElementById("answer_field").innerHTML = "Yes";}
+if (number===9){document.getElementById("answer_field").innerHTML = "Signs point to yes";}
+if (number===10) {document.getElementById("answer_field").innerHTML = "Reply hazy try again";}
+if (number===11) {document.getElementById("answer_field").innerHTML = "Ask again later";}
+if (number===12) {document.getElementById("answer_field").innerHTML = "Better not tell you now";}
+if (number===13) {document.getElementById("answer_field").innerHTML = "Cannot predict now";}
+if (number===14) {document.getElementById("answer_field").innerHTML = "Concentrate and ask again";}
+if (number===15) {document.getElementById("answer_field").innerHTML = "Don't count on it";}
+if (number===16) {document.getElementById("answer_field").innerHTML = "My reply is no";}
+if (number===17) {document.getElementById("answer_field").innerHTML = "My sources say no";}
+if (number===18) {document.getElementById("answer_field").innerHTML = "Outlook not so good";}
+if (number===19) {document.getElementById("answer_field").innerHTML = "Very doubtful";}
+
+document.getElementById("question").value = "";}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
